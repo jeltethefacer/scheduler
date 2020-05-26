bb83471351d6e00c9804549bc5690e7e363856e3
Jelte Boelens <j.w.r.boelens@gmail.com>
2020-05-26 23:39:12 +0200

    papa please

--- controller/login.js
+++ controller/login.js
@@ -8,12 +8,12 @@ const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
   const body = request.body
-
+  console.log(body)
   const user = await User.findOne({ email: body.email })
   const passwordCorrect = user === null
     ? false
     : await bcrypt.compare(body.password, user.passwordHash)
-
+  console.log(passwordCorrect)
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
       errorCode: 'INVALID_EMAIL_OR_PASS'
@@ -24,9 +24,9 @@ loginRouter.post('/', async (request, response) => {
     email: user.email,
     id: user._id,
   }
-
+  console.log(userForToken)
   const token = jwt.sign(userForToken, config.SECRET)
-
+  console.log(token)
   response
     .status(200)
     .send({ token, email: user.email, frontName: user.frontName })
