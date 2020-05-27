988321e034cbd3b975193fc4d15c0e5588fe1fbd
Jelte Boelens <j.w.r.boelens@gmail.com>
2020-05-27 21:11:38 +0200

    deply commit

---
+++ build/precache-manifest.1b36a7585ae102a01f5502491f505fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5033a1e280edd204029241ff672ab973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00983b06cb9796cfc7b8",
+    "url": "/static/css/main.ef10e5f3.chunk.css"
+  },
+  {
+    "revision": "340bd7dee2dddc9bd37d",
+    "url": "/static/js/2.8bc69714.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8bc69714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00983b06cb9796cfc7b8",
+    "url": "/static/js/main.e6679d52.chunk.js"
+  },
+  {
+    "revision": "d54edb572c0fe66d3251",
+    "url": "/static/js/runtime-main.5b832ab2.js"
+  }
+]);
